62ec2a8f1f58f483b9d518465225c94b625e2b8b
SaurabhMulay999 <90036775+SaurabhMulay999@users.noreply.github.com>
2023-10-01 12:55:22 +0530

    update Song Card Component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -10,12 +10,22 @@ const SongCard = ({ song, index }) => {
       <div className="relative w-full h-56 group">
         <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
           <playPause/>
-
         </div>
         <img alt="songimg" src={song.images?.coverart}/>
 
       </div>
-
+      <div className="mt-4 flex flex-col ">
+        <p className="font-semibold text-lg text-white truncate">
+          <Link to={`/song/${song?.key}`}>
+            {song.title}
+          </Link>
+        </p>
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link to={song.artist ?`/artists/${song?.artists[0]?.adamid}`:`/top-artists`}>
+            {song.subtitle}
+          </Link>
+        </p>
+      </div>
     </div>
   )
 };
